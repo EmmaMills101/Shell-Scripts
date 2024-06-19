09b58dcd30550825499157b767bc530051b16a03
Emma <45573808+EmmaMills101@users.noreply.github.com>
2024-06-19 19:59:25 -0400

    filter_contigs_length.sh

--- filter_contigs_length.sh
+++ filter_contigs_length.sh
@@ -1,25 +1,34 @@
 #!/bin/bash
 
-# Activate bbmap
-source activate bbmap
+# Check if the input directory is provided as an argument
+if [ -z "$1" ]; then
+    echo "Usage: $0 /path/to/input"
+    exit 1
+fi
+
+# Set the input directory from the argument
+input_directory="$1"
 
 # Define the minimum length
 min_length=200
 
-# Specify the directory containing your input FASTA files
-input_directory="./Emma_Assemblies"
-
 # Specify the directory where you want to save the filtered FASTA files
 output_directory="./200_filtered"
 
+# Create the output directory if it doesn't exist
+mkdir -p "$output_directory"
+
+# Activate bbmap
+source activate bbmap
+
 # Iterate over the input files
-for input_file in "$input_directory"/*.fna; do
+for input_file in "$input_directory"/*.fasta; do
     # Get the filename without the path and extension
     filename=$(basename -- "$input_file")
     filename_no_ext="${filename%.*}"
 
     # Define the output file path
-    output_file="$output_directory/${filename_no_ext}_filtered.fna"
+    output_file="$output_directory/${filename_no_ext}_filtered.fasta"
 
     # Use reformat.sh to filter contigs
     reformat.sh in="$input_file" out="$output_file" minlength="$min_length"
